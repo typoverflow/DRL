fcab2a6dd20e1e2bda9db565ef17c81b6edd9153
181220014-Gao Chenxiao <1910342119@qq.com>
2020-08-26 23:45:29 +0800

    DQN: buggy commit

--- DQN/README.md
+++ DQN/README.md
@@ -2,3 +2,35 @@
 ## 相关文献
 + [Playing Atari with Deep Reinforcement Learning](./asset/DQN1.pdf)
 + [Human-level control through deep reinforcement learning](./asset/DQN2.pdf)
+
+## Playing Atari with Deep Reinforcement Learning
+#### 论文总结
++ idea
+  + 区分了RL和SL在应用DL时的不同之处
+  + 提出了使用Deep Q-Networks来近似大规模状态空间的Q函数的算法
+    + 其中，Q值网络为$Q:\mathcal{S}\rightarrow \mathbb{R}^{|\mathcal{A}|} $的映射，只需要输入状态即可得到所有行动的Q值
+    + 此处状态的定义实际上是之前4个时间片被预处理后的图像
+    + 优化时使用了随机梯度下降和semi-gradient思想
+  + 提出了使用replay buffer来构造训练数据的想法
+    + 由于replay buffer在所有之前的经验中随机采样，可以避免因为连续采样造成的样本之间的相关性
+    + 每个样本均会多次使用，提高了sample efficiency
+    + 避免因为最近的决策导致的样本上的主导性。比如，如果在某个时间片行动left对应的Q值最高，那么在接下来的采样中样本将被向左的行动所主导
++ 改进方向
+  + replay buffer中，由于在所有的历史经验中随机采样，因而对关键样本的关注度不够。论文作者提出可使用prioritized queue进行采样
++ 实验环境
+  + 作者在Atari 2600平台上进行了实验
+  + 提出了两种度量
+    + average episodic return，即在每个度量时间点处模拟运行计算total reward，然后取平均
+    + estimated Q，即在每个度量时间点处计算所有状态处最大的Q值，然后对状态数目取平均，这一度量比第一种度量更加平滑
++ 算法伪代码  
+  ![](img/2020-08-24-10-36-30.png)
+  + Equation 3: (semi-gradient)
+  $$L_i(\theta_i) = \mathbb{E}_{s, a\sim \rho(\cdot)}[(y_i-Q(s, a|\theta_i))^2] $$
+  $$\nabla_{\theta_t}L_t(\theta_i) = \mathbb{E}_{s, a\sim\rho(\cdot),s'\sim \mathcal{E}}\left[\left(r+\gamma\max_{a'}Q(s', a'|\theta_{i-1})-Q(s, a|\theta_i)\nabla_{\theta_i}Q(s, a|\theta_i)\right)\right] $$
+
+## Human-level control through deep reinforcement learning
++ idea
+  + 感觉上和之前一篇没有太大差别
+  + 引入了使用target Q进行更新的概念。即计算TD误差时，估算$s_{t+1}$的Q值时使用一个旧版本的网络$ \hat{Q} $，然后每过C个time steps更新$ \hat{Q} $到当前的Q。通过“冻结”$ \hat{Q} $而不是使用自益，可以缓解Agent状态的预测值和目标值之间关联性过强的问题
++ 伪代码  
+  ![](img/2020-08-24-23-33-21.png)
---
+++ DQN/src/DQN.py
@@ -0,0 +1,251 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import gym
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+import copy
+
+
+preprocess = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1), 
+    transforms.Resize((96, 80)), 
+    transforms.CenterCrop(84), 
+    # transforms.ToTensor(), 
+])
+
+def preprocessing(frame):
+    frame = Image.fromarray(frame)
+    frame = preprocess(frame)
+    return np.asarray(frame, dtype='float64')
+
+class Paras(object):
+    learning_rate = 0.01
+    batch_size = 32
+    gamma = 0.99
+    buffer_capacity = 10000
+    niteration = 100
+    agent_hist = 4
+
+    init_epsilon = 1
+    final_epsilon = 0.1
+    epsilon_decay = 1/100000
+
+    target_update_freq = 100
+
+    npixel = 84*84
+    lpixel = 84
+
+class Env(object):
+    env = gym.make("SpaceInvaders-v0")
+    frame_shape = env.observation_space.shape
+    action_num = env.action_space.n
+
+    crop_shape = (84, 84)
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=(8, 8), stride=4)
+        self.conv1.weight.data.normal_(0, 0.1)
+
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(4, 4), stride=2)
+        self.conv2.weight.data.normal_(0, 0.1)
+
+        self.hidden3 = nn.Linear(81*32, 256)
+        self.hidden3.weight.data.normal_(0, 0.1)
+        self.hidden3.bias.data.normal_(0, 0.1)
+
+        self.output = nn.Linear(256, Env.action_num, bias=False)
+        self.output.weight.data.normal_(0, 0.1)
+
+
+    def forward(self, X):
+        """
+            input: input of shape (Minibatch, 4, 84, 84)
+            output: tensor of shape(Minibatch, action_num)
+        """
+        X = F.relu(self.conv1(X))
+        X = F.relu(self.conv2(X))
+        X = X.view(X.size(0), -1)
+        X = self.hidden3(X)
+        action_values = self.output(X)
+        return action_values
+
+    
+class DQN():
+    def __init__(self):
+        self.lr = Paras.learning_rate
+        self.gamma = Paras.gamma
+        
+        self.epsilon = self.init_epsilon = Paras.init_epsilon
+        self.final_epsilon = Paras.final_epsilon
+        self.epsilon_decay = Paras.epsilon_decay
+
+        self.buffer_cnt = 0
+        self.buffer_cur = 0
+        self.buffer = np.zeros((Paras.buffer_capacity, 1+1+8*Env.crop_shape[0] * Env.crop_shape[1]))
+        
+
+        self.target_net = self.eval_net = SimpleCNN()
+
+        self.global_step = 0
+
+        self.recent_frames = np.zeros((4, 84, 84))
+        self.recent_cur = 0
+        self.recent_cnt = 0
+
+        self.optimizer = torch.optim.SGD(self.eval_net.parameters(), lr=self.lr)
+        self.loss_func = nn.MSELoss("mean")
+
+    def choose_action(self, frame):
+        if np.random.random() < self.epsilon:
+            return np.random.randint(0, Env.action_num)
+        else:
+            tmp = np.vstack([
+                self.recent_frames[(self.recent_cur-3)%4][None, :], 
+                self.recent_frames[(self.recent_cur-2)%4][None, :], 
+                self.recent_frames[(self.recent_cur-1)%4][None, :], 
+                frame[None, :]
+            ])
+            action_values = self.eval_net.forward(torch.FloatTensor(tmp[None, :]))[0]
+            return action_values.argmax()
+
+    def store_transition(self, frame, action, reward, next_frame):
+        self.recent_frames[self.recent_cur] = frame
+        self.recent_cnt += 1
+        self.recent_cur = (self.recent_cur+1)%4
+
+        transition = np.hstack([
+            self.recent_frames[(self.recent_cur-4)%4].flatten(), 
+            self.recent_frames[(self.recent_cur-3)%4].flatten(), 
+            self.recent_frames[(self.recent_cur-2)%4].flatten(), 
+            self.recent_frames[(self.recent_cur-1)%4].flatten(), 
+            self.recent_frames[(self.recent_cur-3)%4].flatten(), 
+            self.recent_frames[(self.recent_cur-2)%4].flatten(), 
+            self.recent_frames[(self.recent_cur-1)%4].flatten(), 
+            next_frame.flatten(), 
+            np.asarray([action, reward])
+        ])
+
+        self.buffer[self.buffer_cur] = transition
+        self.buffer_cnt += 1
+        self.buffer_cur = (self.buffer_cur+1)%Paras.buffer_capacity
+
+        return transition
+
+    def learn(self):
+        if self.global_step % Paras.target_update_freq == 0:
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+
+        self.global_step += 1
+        if self.epsilon > self.final_epsilon:
+            self.epsilon -= self.epsilon_decay
+
+        sample_index = np.random.choice(min(self.buffer_cnt, Paras.buffer_capacity), size=Paras.batch_size)
+        batch = self.buffer[sample_index, :]
+        batch_state = np.reshape(batch[:, :4*84*84], (-1, 4, 84, 84))
+        batch_state_prime = np.reshape(batch[:, 4*84*84:2*4*84*84], (-1, 4, 84, 84))
+        batch_action = batch[:, -2]
+        batch_reward = batch[:, -1]
+
+        batch_state = torch.FloatTensor(batch_state)
+        batch_state_prime = torch.FloatTensor(batch_state_prime)
+        batch_action = torch.LongTensor(batch_action.astype(int)[:, None])
+        batch_reward = torch.FloatTensor(batch_reward[:, None])
+
+        q_eval = self.eval_net(batch_state).gather(1, batch_action)
+        q_next = self.target_net(batch_state).detach()
+        q_target = batch_reward + self.gamma * q_next.max(1)[0].view(Paras.batch_size, 1)
+
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def clear_recent(self):
+        self.recent_cur = self.recent_cnt = 0
+        self.recent_frames = np.zeros((4, 84, 84))
+
+
+def main():
+    dqn = DQN()
+    episodes = 400
+    reward_list = []
+    plt.ion()
+    print("Start training...")
+    for i in range(episodes):
+        frame = Env.env.reset()
+        frame = preprocessing(frame)
+        # plt.imshow(frame)
+        total_reward = 0
+        while True:
+            # Env.env.render()
+            action = dqn.choose_action(frame)
+            next_frame, reward, done, _ = Env.env.step(action)
+            next_frame = preprocessing(next_frame)
+
+            dqn.store_transition(frame, action, reward, next_frame)
+
+            total_reward += reward
+            if dqn.buffer_cnt >= Paras.buffer_capacity:
+                dqn.learn()
+            
+            if done:
+                print("finish episode {}, total reward is {}.".format(i, total_reward))
+                break
+            
+            frame = copy.deepcopy(next_frame)
+        
+        reward_list.append(total_reward)
+    
+    plt.plot(reward_list)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__": 
+    pass
+
+    plt.imshow()
